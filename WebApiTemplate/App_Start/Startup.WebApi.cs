e06081d706df8b5344c60a385433f207b3d2dd19
Justin Ronco <jnonce@outlook.com>
2015-06-12 07:21:26 -0700

    Validation system updates (DI)

--- WebApiTemplate/App_Start/Startup.WebApi.cs
+++ WebApiTemplate/App_Start/Startup.WebApi.cs
@@ -1,10 +1,15 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
 using FluentValidation.WebApi;
 using Owin;
 using webapitmpl.Configuration;
 using webapitmpl.Utility;
+using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace webapitmpl.App_Start
 {
@@ -28,7 +33,11 @@ namespace webapitmpl.App_Start
             config.MapHttpAttributeRoutes();
 
             // Validation            
-            FluentValidationModelValidatorProvider.Configure(config);
+            FluentValidationModelValidatorProvider.Configure(config,
+                provider =>
+                {
+                    provider.ValidatorFactory = new AutofacFluentValidatorFactory(container);
+                });
 
             // Pull the OWIN dependency scope into WebApi's request state
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
@@ -47,7 +56,38 @@ namespace webapitmpl.App_Start
             builder.RegisterHttpRequestMessage(config);
 
             // Autofac will create controllers
-            builder.RegisterApiControllers(System.Reflection.Assembly.GetExecutingAssembly());
+            var asm = System.Reflection.Assembly.GetExecutingAssembly();
+            builder.RegisterApiControllers(asm);
+
+            //
+            builder.RegisterAssemblyTypes(asm)
+                .Where(t => typeof(FluentValidation.IValidator).IsAssignableFrom(t))
+                .As(type => GetTypes(type))
+                ;
+        }
+
+        private static IEnumerable<Type> GetTypes(Type validatorType)
+        {
+            var interfaces = new List<Type>();
+
+            // Match the convention on the validator name
+            Match match = Regex.Match(validatorType.Name, @"(\w+)Validator", RegexOptions.CultureInvariant);
+            if (match.Success)
+            {
+                // Locate the targetType
+                string targetType = String.Format("{0}.{1}", validatorType.Namespace, match.Groups[1].Value);
+                Type type = validatorType.Assembly.GetType(targetType, throwOnError: false);
+                if (type != null)
+                {
+                    Type desiredType = typeof(IValidator<>).MakeGenericType(type);
+                    if (desiredType.IsAssignableFrom(validatorType))
+                    {
+                        interfaces.Add(desiredType);
+                    }
+                }
+            }
+
+            return interfaces;
         }
     }
 }
